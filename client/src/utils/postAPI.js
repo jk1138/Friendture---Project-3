358c6693ff68d5acd8b3a048a23234c2eb95c257
jyk <47995007+jk1138@users.noreply.github.com>
2019-10-31 20:18:15 -0400

    added userpage info and comments

--- client/src/utils/postAPI.js
+++ client/src/utils/postAPI.js
@@ -3,5 +3,21 @@ import axios from "axios";
 export default {
     getPosts: function(){
         return axios.get("/api/posts");
+    };
+
+    findUser: function (userData) {
+    return axios.post("/api/users/login", userData);
     },
-}
+    // Saves a user to the database
+    saveUser: function (userData) {
+    return axios.post("/api/users", userData);
+    },
+
+  getComments: function(commentData){
+    return axios.post("/api/comment/grab", commentData);
+  },
+  postComment: function(commentData){
+    console.log(commentData);
+    return axios.post("/api/comment/release", commentData);
+  }
+};
