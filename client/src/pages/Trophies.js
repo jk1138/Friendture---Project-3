81f9378815b015f5272fa2efdbb3b2319e5a45d3
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-11-05 13:30:46 -0500

    added 3 more trophies to json and trophy cards; changed css of cards; added the ha ndle click functions of the 3 new trophies

--- client/src/pages/Trophies.js
+++ client/src/pages/Trophies.js
@@ -6,6 +6,9 @@ import "./trophies.css";
 import image1 from "../utils/images/gotPizzaTrophy.png";
 import image2 from "../utils/images/selfieTrophy.png";
 import image3 from "../utils/images/gymTrophy.png";
+import image4 from "../utils/images/campingTrophy.png";
+import image5 from "../utils/images/moviesTrophy.png";
+import image6 from "../utils/images/roadTripTrophy.png";
 
 class Trophy extends Component {
 
@@ -23,7 +26,9 @@ class Trophy extends Component {
     this.handleClick1 = this.handleClick1.bind(this);
     this.handleClick2 = this.handleClick2.bind(this);
     this.handleClick3 = this.handleClick3.bind(this);
-
+    this.handleClick4 = this.handleClick4.bind(this);
+    this.handleClick5 = this.handleClick5.bind(this);
+    this.handleClick6 = this.handleClick6.bind(this);
 
   }
 
@@ -33,9 +38,9 @@ class Trophy extends Component {
 
     if (this.state.isFlipped1) {
       this.setState({ isFlipped1: !this.state.isFlipped1 });
-    } else if(!this.state.isFlipped1) {
+    } else if (!this.state.isFlipped1) {
       this.setState({ isFlipped1: !this.state.isFlipped1 });
-    }  
+    }
   };
 
   handleClick2(e) {
@@ -43,9 +48,9 @@ class Trophy extends Component {
 
     if (this.state.isFlipped2) {
       this.setState({ isFlipped2: !this.state.isFlipped2 });
-    } else if(!this.state.isFlipped2) {
+    } else if (!this.state.isFlipped2) {
       this.setState({ isFlipped2: !this.state.isFlipped2 });
-    }   
+    }
   };
 
   handleClick3(e) {
@@ -53,41 +58,69 @@ class Trophy extends Component {
 
     if (this.state.isFlipped3) {
       this.setState({ isFlipped3: !this.state.isFlipped3 });
-    } else if(!this.state.isFlipped3) {
+    } else if (!this.state.isFlipped3) {
       this.setState({ isFlipped3: !this.state.isFlipped3 });
     }
   };
 
-  
 
+  handleClick4(e) {
+    e.preventDefault();
+
+    if (this.state.isFlipped4) {
+      this.setState({ isFlipped4: !this.state.isFlipped4 });
+    } else if (!this.state.isFlipped4) {
+      this.setState({ isFlipped4: !this.state.isFlipped4 });
+    }
+  };
+
+  handleClick5(e) {
+    e.preventDefault();
+
+    if (this.state.isFlipped5) {
+      this.setState({ isFlipped5: !this.state.isFlipped5 });
+    } else if (!this.state.isFlipped5) {
+      this.setState({ isFlipped5: !this.state.isFlipped5 });
+    }
+  };
+
+  handleClick6(e) {
+    e.preventDefault();
+
+    if (this.state.isFlipped6) {
+      this.setState({ isFlipped6: !this.state.isFlipped6 });
+    } else if (!this.state.isFlipped6) {
+      this.setState({ isFlipped6: !this.state.isFlipped6 });
+    }
+  };
 
   render() {
     return (
-
-      <div><div className="border addMargin">
-
-        <ReactCardFlip className="border" isFlipped={this.state.isFlipped1} flipSpeedFrontToBack={1.0}
-          flipSpeedBackToFront={1.0} flipDirection="horizontal">
-          <TrophyImages
-            image={image1}
-            name={"Got pizza!"}
-            className="border" key="front">
-            This is the front of the card.
+      <div>
+        {/* Start Pizza !!!! */}
+        <div className="border addMargin">
+          <ReactCardFlip className="border" isFlipped={this.state.isFlipped1} flipSpeedFrontToBack={1.0}
+            flipSpeedBackToFront={1.0} flipDirection="horizontal">
+            <TrophyImages className="img"
+              image={image1}
+              name={"Got pizza!"}
+              className="border" key="front">
+              This is the front of the card.
           </TrophyImages>
 
-          <TrophyEmailForm className="border" key="back"
-          >
-            This is the back of the card.
+          <TrophyEmailForm className="border" key="back">
+              This is the back of the card.
           </TrophyEmailForm>
-        </ReactCardFlip>
-        <button className="border" onClick={this.handleClick1} id="pizza">Click to flip</button>
-      </div>
+          </ReactCardFlip>
+          <button onClick={this.handleClick1} id="pizza">Fill out trophy form</button>
+        </div>
+        {/* End Pizza !!!! */}
 
+        {/* Start Selfie!!!! */}
         <div className="border addMargin">
-
-          <ReactCardFlip className="border" isFlipped={this.state.isFlipped2} flipSpeedFrontToBack={1.0}
+          <ReactCardFlip isFlipped={this.state.isFlipped2} flipSpeedFrontToBack={1.0}
             flipSpeedBackToFront={1.0} flipDirection="horizontal">
-            <TrophyImages
+            <TrophyImages className="img"
               image={image2}
               alt={"cartoon photo of polaroid camera"}
               name={"Took a selfie together!"}
@@ -100,11 +133,12 @@ class Trophy extends Component {
               This is the back of the card.
           </TrophyEmailForm>
           </ReactCardFlip>
-          <button className="border" onClick={this.handleClick2}>Click to flip</button>
+          <button onClick={this.handleClick2}>Click to flip</button>
         </div>
+        {/* End Selfie!!!! */}
 
+        {/* Start Gym!!!! */}
         <div className="border addMargin">
-
           <ReactCardFlip className="border" isFlipped={this.state.isFlipped3} flipSpeedFrontToBack={1.0}
             flipSpeedBackToFront={1.0} flipDirection="horizontal">
             <TrophyImages
@@ -115,13 +149,78 @@ class Trophy extends Component {
               This is the front of the card.
           </TrophyImages>
 
+            <TrophyEmailForm className="border" key="back">
+              This is the back of the card.
+          </TrophyEmailForm>
+          </ReactCardFlip>
+          <button onClick={this.handleClick3}>Click to flip</button>
+        </div>
+        {/* End Gym!!!! */}
+
+        {/* Start Camping!!!! */}
+        <div className="border addMargin">
+          <ReactCardFlip className="border" isFlipped={this.state.isFlipped4} flipSpeedFrontToBack={1.0}
+            flipSpeedBackToFront={1.0} flipDirection="horizontal">
+            <TrophyImages
+              image={image4}
+              alt={"clipart of yellow tent"}
+              name={"Went on the coolest camping trip!"}
+              className="border" key="front">
+              This is the front of the card.
+          </TrophyImages>
+
             <TrophyEmailForm className="border" key="back"
             >
               This is the back of the card.
           </TrophyEmailForm>
           </ReactCardFlip>
-          <button className="border" onClick={this.handleClick3}>Click to flip</button>
+          <button onClick={this.handleClick4}>Click to flip</button>
         </div>
+        {/* End Camping !!!! */}
+
+        {/* Start Movies!!!! */}
+        <div className="border addMargin">
+          <ReactCardFlip className="border" isFlipped={this.state.isFlipped5} flipSpeedFrontToBack={1.0}
+            flipSpeedBackToFront={1.0} flipDirection="horizontal">
+            <TrophyImages
+              image={image5}
+              alt={"clipart of 2 movie tickets"}
+              name={"Saw a movie!!"}
+              className="border" key="front">
+              This is the front of the card.
+          </TrophyImages>
+
+            <TrophyEmailForm className="border" key="back"
+            >
+              This is the back of the card.
+          </TrophyEmailForm>
+          </ReactCardFlip>
+          <button onClick={this.handleClick5}>Click to flip</button>
+        </div>
+        {/* End Movies !!!! */}
+
+        {/* Start Road Trip!!!! */}
+        <div className="border addMargin">
+          <ReactCardFlip className="border" isFlipped={this.state.isFlipped6} flipSpeedFrontToBack={1.0}
+            flipSpeedBackToFront={1.0} flipDirection="horizontal">
+            <TrophyImages
+              image={image6}
+              alt={"clipart of a camper"}
+              name={"Saw some sights on a road trip!"}
+              className="border" key="front">
+              This is the front of the card.
+          </TrophyImages>
+
+            <TrophyEmailForm className="border" key="back"
+            >
+              This is the back of the card.
+          </TrophyEmailForm>
+          </ReactCardFlip>
+          <button onClick={this.handleClick6}>Click to flip</button>
+        </div>
+        {/* End Road Trip !!!! */}
+
+
       </div>
     )
 
