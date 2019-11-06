1ebf2cff8e33d76811cf61df558436214452708e
jyk <47995007+jk1138@users.noreply.github.com>
2019-11-06 17:15:13 -0500

    more commenting out for comment

--- README.md
+++
@@ -1,3 +0,0 @@
-
-href="https://codepen.io/annamatic3000/#" target="_blank"> Anna Lee as a response to a challenge from 
-href="https://www.freecodecamp.com/" target="_blank">FreeCodeCamp
--- client/src/utils/postAPI.js
+++ client/src/utils/postAPI.js
@@ -17,9 +17,7 @@ export default {
   saveUser: function (userData) {
     return axios.post("/api/users", userData);
   },
-  findTrophies: function (userData) {
-    return axios.get("/api/users", userData);
-  },
+
   // getComments: function (commentData) {
   //   return axios.post("/api/comment/grab", commentData);
   // },
